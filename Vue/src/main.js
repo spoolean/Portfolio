40803e3daaf79abd3bc900c2d4502ef88448db3a
James Roche <jroche@lincoln.ac.uk>
2022-08-03 17:28:55 +0100

    Add Bootstrap to Vue

--- Vue/src/main.js
+++ Vue/src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from 'vue'
+import Vuex from 'vuex'
+import BootstrapVue from 'bootstrap-vue'
 
-createApp(App).mount('#app')
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/app.scss'
+
+import App from './App.js'
+import Store from './store.js'
+
+Vue.use(BootstrapVue);
+Vue.use(Vuex);
+
+new Vue({
+    store: Store,
+    render: (h) => h(App),
+}).$mount("#app");
